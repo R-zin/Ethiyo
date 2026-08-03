4da9232861db3a15592d5613f5e1a0489401e084
R-zin <razinm24bcs24@iiitkottayam.ac.in>
2026-08-04 00:06:31 +0530

    Added health check

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	router := gin.Default()
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "ok",
+		})
+	})
+	err := router.Run(":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
