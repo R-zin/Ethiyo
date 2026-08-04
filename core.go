75d4bfd533ce780da39063752ebd03e6237adfa7
R-zin <razinm24bcs24@iiitkottayam.ac.in>
2026-08-04 19:03:52 +0530

    add: webscrapper

--- core.go
+++ core.go
@@ -4,13 +4,30 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"github.com/gocolly/colly"
 )
 
+func scrap(url string) {
+	c := colly.NewCollector()
+	c.OnHTML("body", func(e *colly.HTMLElement) {})
+}
+
+
 func request(BusCode string) {
+
 	url := fmt.Sprintf("https://chalo.com/app/public-route/%s", BusCode)
-	resp, err := http.Get(url)
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/26.5.2 Safari/605.1.15")
+	client := &http.Client{}
+	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(resp.Body)
+	fmt.Println(resp.Body.Read())
+	defer resp.Body.Close()
+
+func ge_bus_route(Bus_url string) {
+
+
+	}
 }
